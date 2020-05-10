c9a538746e6b5d7e928d931002800fb9d7b20e7d
SergiOn <onishhenko@gmail.com>
2020-05-10 19:07:21 +0300

    #ios-13-app-development-bootcamp # Section 2: Xcode Storyboards and Interface Builder # 17. Let's Create a Brand New Xcode Project

---
+++ Courses/iOS-13-Swift-5-The-Complete-iOS-App-Development-Bootcamp/Development/I Am Rich/I Am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Serhii Onyshchenko on 10.05.2020.
+//  Copyright © 2020 Serhii Onyshchenko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
