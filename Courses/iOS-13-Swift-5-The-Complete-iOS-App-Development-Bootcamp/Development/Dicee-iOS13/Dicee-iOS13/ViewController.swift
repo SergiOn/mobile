48aaaf1fb5f0aaa9d38f5373465617bcab5afb0c
SergiOn <onishhenko@gmail.com>
2020-05-11 23:23:37 +0300

    #ios-13-app-development-bootcamp # Section 4: Swift Programming Basics - Collections, Constants & Variables # 42. Storing Data using Variables and Arrays

--- Courses/iOS-13-Swift-5-The-Complete-iOS-App-Development-Bootcamp/Development/Dicee-iOS13/Dicee-iOS13/ViewController.swift
+++ Courses/iOS-13-Swift-5-The-Complete-iOS-App-Development-Bootcamp/Development/Dicee-iOS13/Dicee-iOS13/ViewController.swift
@@ -13,19 +13,24 @@ class ViewController: UIViewController {
     @IBOutlet weak var diceImageView1: UIImageView!
     @IBOutlet weak var diceImageView2: UIImageView!
     
+    var leftDiceNumber = 1
+    var rightDiceNumber = 5
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
-        diceImageView1.image = #imageLiteral(resourceName: "DiceSix")        
+        diceImageView1.image = #imageLiteral(resourceName: "DiceSix")
         diceImageView2.image = #imageLiteral(resourceName: "DiceTwo")
     }
 
     @IBAction func rollButtonPressed(_ sender: UIButton) {
-        print("Button got tapped.")
-        diceImageView1.image = #imageLiteral(resourceName: "DiceFour")
-        diceImageView2.image = #imageLiteral(resourceName: "DiceFour")
-    }
+        diceImageView1.image = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")][leftDiceNumber]
+        diceImageView2.image = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")][rightDiceNumber]
     
+        leftDiceNumber = leftDiceNumber + 1
+        rightDiceNumber = rightDiceNumber - 1
+    }
+
 }
 
