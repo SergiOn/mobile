3afa678a688d9ab25ece171b335072a96aab0b76
SergiOn <onishhenko@gmail.com>
2020-05-15 19:40:40 +0300

    #ios-13-app-development-bootcamp # Section 8: Intermediate Swift Programming - Control Flow and Optionals # 96. Showing the Timer to the User with a Progress View

--- Courses/iOS-13-Swift-5-The-Complete-iOS-App-Development-Bootcamp/Development/EggTimer-iOS13/EggTimer/ViewController.swift
+++ Courses/iOS-13-Swift-5-The-Complete-iOS-App-Development-Bootcamp/Development/EggTimer-iOS13/EggTimer/ViewController.swift
@@ -11,7 +11,8 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var titleLabel: UILabel!
-
+    @IBOutlet weak var progressBar: UIProgressView!
+    
     let eggTimes = ["Soft": 3, "Medium": 420, "Hard": 720]
     let defaultSecondsRemaining = 60
     var timer: Timer?
@@ -19,6 +20,8 @@ class ViewController: UIViewController {
     @IBAction func hardnessSelected(_ sender: UIButton) {
         let hardness: String = sender.currentTitle!
         var secondsRemaining = eggTimes[hardness] ?? defaultSecondsRemaining
+
+        progressBar.setProgress(1.0, animated: false)
 
         timer?.invalidate()
         timer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { (Timer) in
