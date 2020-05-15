618d32d320a48fd1286538452a9a9f0baed23ac5
SergiOn <onishhenko@gmail.com>
2020-05-15 20:55:24 +0300

    #ios-13-app-development-bootcamp # Section 8: Intermediate Swift Programming - Control Flow and Optionals # 99. Download the Completed App Project

--- Courses/iOS-13-Swift-5-The-Complete-iOS-App-Development-Bootcamp/Development/EggTimer-iOS13/EggTimer/ViewController.swift
+++ Courses/iOS-13-Swift-5-The-Complete-iOS-App-Development-Bootcamp/Development/EggTimer-iOS13/EggTimer/ViewController.swift
@@ -7,15 +7,18 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var progressBar: UIProgressView!
     
-    let eggTimes = ["Soft": 3, "Medium": 42, "Hard": 72]
+    let eggTimes = ["Soft": 300, "Medium": 420, "Hard": 720]
     let defaultSecondsRemaining = 60
+
     var timer: Timer?
+    var player: AVAudioPlayer!
 
     @IBAction func hardnessSelected(_ sender: UIButton) {
         let hardness: String = sender.currentTitle!
@@ -27,19 +30,23 @@ class ViewController: UIViewController {
         titleLabel.text = hardness
 
         timer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { (Timer) in
-            print(totalTime)
-            print(secondsPassed)
-
             if secondsPassed < totalTime {
                 secondsPassed += Int(Timer.timeInterval)
+
+                let percentageProgress = Float(secondsPassed) / Float(totalTime)
+                self.progressBar.setProgress(percentageProgress, animated: true)
             } else {
                 Timer.invalidate()
                 self.titleLabel.text = "DONE!"
+                self.playSound()
             }
-            let percentageProgress = Float(secondsPassed) / Float(totalTime)
-            self.progressBar.setProgress(percentageProgress, animated: true)
-            print(percentageProgress)
         }
+    }
+
+    func playSound() {
+        let url = Bundle.main.url(forResource: "alarm_sound", withExtension: "mp3")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
     }
 
 }
