bc901f7cedc395388d80eb5f9f4a2155d0dc1135
SergiOn <onishhenko@gmail.com>
2020-06-06 17:26:01 +0300

    #ios-13-app-development-bootcamp # Section 11: Advanced Swift Programming - Classes, Inheritance & Advanced Optionals # 133. How to Use the Docs in Xcode and Create a Custom UIViewController Class

---
+++ Courses/iOS-13-Swift-5-The-Complete-iOS-App-Development-Bootcamp/Development/BMI-Calculator-iOS13/BMI Calculator/Controllers/SecondViewController.swift
@@ -0,0 +1,16 @@
+//
+//  SecondViewController.swift
+//  BMI Calculator
+//
+//  Created by Serhii Onyshchenko on 06.06.2020.
+//  Copyright © 2020 Angela Yu. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
