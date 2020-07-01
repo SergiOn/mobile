71ebe455c21b5f746e64b4f579a771543f54dd73
SergiOn <onishhenko@gmail.com>
2020-07-01 22:48:27 +0300

    #ios-13-app-development-bootcamp # Section 13: Networking, JSON Parsing, APIs and Core Location # 160. Use the URLSession for Networking

--- Courses/iOS-13-Swift-5-The-Complete-iOS-App-Development-Bootcamp/Development/Clima-iOS13/Clima/WeatherManager.swift
+++ Courses/iOS-13-Swift-5-The-Complete-iOS-App-Development-Bootcamp/Development/Clima-iOS13/Clima/WeatherManager.swift
@@ -9,10 +9,41 @@
 import Foundation
 
 struct WeatherManager {
-    let weatherUrl = "http://api.openweathermap.org/data/2.5/weather?appid=d0da1369f036fee05336c59a07bc2381&units=metric"
+    let weatherUrl = "https://api.openweathermap.org/data/2.5/weather?appid=d0da1369f036fee05336c59a07bc2381&units=metric"
     
     func fetchWeather(_ cityName: String) {
-        let url = "\(weatherUrl)&q=\(cityName)"
-        print(url)
+        let urlString = "\(weatherUrl)&q=\(cityName)"
+        print(urlString)
+        
+        performRequest(urlString)
     }
+    
+    func performRequest(_ urlString: String) {
+        // 1. Create a URL
+        if let url = URL(string: urlString) {
+            
+            // 2. Create a URLSession
+            let session = URLSession(configuration: URLSessionConfiguration.default)
+            
+            // 3. Give the session a task
+            let task = session.dataTask(with: url, completionHandler: handle(data: response: error:))
+            
+            // 4. Start the task
+            task.resume()
+        }
+    }
+    
+    func handle(data: Data?, response: URLResponse?, error: Error?) {
+        if error != nil {
+            print(error!)
+            return
+        }
+        
+        if let safeData = data {
+            let dataString = String(data: safeData, encoding: String.Encoding.utf8)
+            print(dataString!)
+            return
+        }
+    }
+
 }
