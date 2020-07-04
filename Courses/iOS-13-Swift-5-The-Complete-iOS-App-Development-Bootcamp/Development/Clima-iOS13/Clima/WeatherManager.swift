991381bd223e7fb650b52eac05abaf0725be5733
SergiOn <onishhenko@gmail.com>
2020-07-04 22:36:18 +0300

    #ios-13-app-development-bootcamp # Section 13: Networking, JSON Parsing, APIs and Core Location # 162. JSON Decoding

--- Courses/iOS-13-Swift-5-The-Complete-iOS-App-Development-Bootcamp/Development/Clima-iOS13/Clima/WeatherManager.swift
+++ Courses/iOS-13-Swift-5-The-Complete-iOS-App-Development-Bootcamp/Development/Clima-iOS13/Clima/WeatherManager.swift
@@ -26,11 +26,36 @@ struct WeatherManager {
             let session = URLSession(configuration: URLSessionConfiguration.default)
             
             // 3. Give the session a task
-            let task = session.dataTask(with: url, completionHandler: handle(data: response: error:))
+//            let task = session.dataTask(with: url, completionHandler: handle(data: response: error:))
+            let task = session.dataTask(with: url) { (data, response, error) in
+                if error != nil {
+                    print(error!)
+                    return
+                }
+                
+                if let safeData = data {
+                    let dataString = String(data: safeData, encoding: String.Encoding.utf8)
+                    print(dataString!)
+                    self.parseJSON(weatherData: safeData)
+                    return
+                }
+            }
             
             // 4. Start the task
             task.resume()
         }
+    }
+    
+    func parseJSON(weatherData: Data) {
+        let decoder = JSONDecoder()
+        do {
+            let decodedData = try decoder.decode(WeatherData.self, from: weatherData)
+            print(decodedData.name)
+            print(decodedData.main.temp)
+        } catch {
+            print(error)
+        }
+        
     }
     
     func handle(data: Data?, response: URLResponse?, error: Error?) {
