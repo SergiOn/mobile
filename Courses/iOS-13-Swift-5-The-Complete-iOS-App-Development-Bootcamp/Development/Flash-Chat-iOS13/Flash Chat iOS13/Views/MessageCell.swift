54a836d2359be6e14cf3a5622d74cedf32c3640f
SergiOn <onishhenko@gmail.com>
2020-07-08 21:13:39 +0300

    #ios-13-app-development-bootcamp # Section 15: Firebase Cloud Firestore, TableViews and Cocoapod Dependencies # 203. Customising Cells in a TableView using a .xib File

---
+++ Courses/iOS-13-Swift-5-The-Complete-iOS-App-Development-Bootcamp/Development/Flash-Chat-iOS13/Flash Chat iOS13/Views/MessageCell.swift
@@ -0,0 +1,29 @@
+//
+//  MessageCell.swift
+//  Flash Chat iOS13
+//
+//  Created by Serhii Onyshchenko on 08.07.2020.
+//  Copyright © 2020 Angela Yu. All rights reserved.
+//
+
+import UIKit
+
+class MessageCell: UITableViewCell {
+    
+    @IBOutlet weak var messageBubble: UIView!
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var rightImageView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        messageBubble.layer.cornerRadius = messageBubble.frame.size.height / 5
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
