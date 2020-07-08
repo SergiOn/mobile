4844807dbbca803c26d368c61fe0f3a4b533a159
SergiOn <onishhenko@gmail.com>
2020-07-08 20:14:54 +0300

    #ios-13-app-development-bootcamp # Section 15: Firebase Cloud Firestore, TableViews and Cocoapod Dependencies # 202. How to use a UITableView and Create a Message Model

---
+++ Courses/iOS-13-Swift-5-The-Complete-iOS-App-Development-Bootcamp/Development/Flash-Chat-iOS13/Flash Chat iOS13/Message.swift
@@ -0,0 +1,14 @@
+//
+//  Message.swift
+//  Flash Chat iOS13
+//
+//  Created by Serhii Onyshchenko on 08.07.2020.
+//  Copyright © 2020 Angela Yu. All rights reserved.
+//
+
+import Foundation
+
+struct Message {
+    let sender: String
+    let body: String
+}
