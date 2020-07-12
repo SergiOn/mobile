d43104c5e200008bd2c8725ea47454a3d57f4287
SergiOn <onishhenko@gmail.com>
2020-07-12 19:58:47 +0300

    #ios-13-app-development-bootcamp # Section 17: SwiftUI and Declarative Programming # 226. SwiftUI Dicee Part 1 - Designing a Layout using Spacers and Subviews

---
+++ Courses/iOS-13-Swift-5-The-Complete-iOS-App-Development-Bootcamp/Development/Dicee-SwiftUI/Dicee-SwiftUI/ContentView.swift
@@ -0,0 +1,58 @@
+//
+//  ContentView.swift
+//  Dicee-SwiftUI
+//
+//  Created by Serhii Onyshchenko on 12.07.2020.
+//  Copyright © 2020 Serhii Onyshchenko. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        ZStack {
+            Text("Hello, World!")
+            Image("background")
+                .resizable()
+                .edgesIgnoringSafeArea(.all)
+            VStack {
+                Image("diceeLogo")
+                Spacer()
+                HStack {
+                    DiceeView(n: 1)
+                    DiceeView(n: 2)
+                }
+                .padding(.horizontal)
+                Spacer()
+                Button(action: {
+                    
+                }) {
+                    Text("Roll")
+                        .font(.system(size: 50))
+                        .fontWeight(.heavy)
+                        .foregroundColor(.white)
+                        .padding(.horizontal)
+                }
+                .background(Color.red)
+            }
+        }
+    }
+}
+
+struct DiceeView: View {
+    
+    let n: Int
+    
+    var body: some View {
+        Image("dice\(n)")
+            .resizable()
+            .aspectRatio(1, contentMode: .fit)
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
