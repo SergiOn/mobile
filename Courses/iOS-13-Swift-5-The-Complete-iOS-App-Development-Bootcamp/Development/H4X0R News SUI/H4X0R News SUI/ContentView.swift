7074329b88da5adca758bf5483ea526193b1bb19
SergiOn <onishhenko@gmail.com>
2020-07-12 20:51:15 +0300

    #ios-13-app-development-bootcamp # Section 17: SwiftUI and Declarative Programming # 229. H4X0R News Part 1 - Understanding the List and Identifiable Protocol

---
+++ Courses/iOS-13-Swift-5-The-Complete-iOS-App-Development-Bootcamp/Development/H4X0R News SUI/H4X0R News SUI/ContentView.swift
@@ -0,0 +1,37 @@
+//
+//  ContentView.swift
+//  H4X0R News SUI
+//
+//  Created by Serhii Onyshchenko on 12.07.2020.
+//  Copyright © 2020 Serhii Onyshchenko. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        NavigationView {
+            List(posts) { post in
+                Text(post.title)
+            }
+            .navigationBarTitle("H4X0R News")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
+struct Post: Identifiable {
+    let id: String
+    let title: String
+}
+
+let posts = [
+    Post(id: "1", title: "Hello"),
+    Post(id: "2", title: "Bonjour"),
+    Post(id: "3", title: "Hola"),
+]
