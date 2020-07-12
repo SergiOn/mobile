d19dab8a727804432ed3dbcefd3694e93e0a9412
SergiOn <onishhenko@gmail.com>
2020-07-12 21:41:08 +0300

    #ios-13-app-development-bootcamp # Section 17: SwiftUI and Declarative Programming # 231. H4X0R News Part 3 - The Observer Design Pattern

--- Courses/iOS-13-Swift-5-The-Complete-iOS-App-Development-Bootcamp/Development/H4X0R News SUI/H4X0R News SUI/models/NetworkManager.swift
+++ Courses/iOS-13-Swift-5-The-Complete-iOS-App-Development-Bootcamp/Development/H4X0R News SUI/H4X0R News SUI/models/NetworkManager.swift
@@ -8,7 +8,9 @@
 
 import Foundation
 
-class NetworkManager {
+class NetworkManager: ObservableObject {
+    
+    @Published var posts = [Post]()
     
     func fetchData() {
         if let url = URL(string: "https://hn.algolia.com/api/v1/search?tags=front_page") {
@@ -20,7 +22,7 @@ class NetworkManager {
                         do {
                             let results = try decoder.decode(Results.self, from: safeData)
                             DispatchQueue.main.async {
-//                                self.posts = results.hits
+                                self.posts = results.hits
                             }
                         } catch {
                             print(error)
