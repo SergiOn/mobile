1b38ff9f353d239d3df07a9f10fb788256540a04
SergiOn <onishhenko@gmail.com>
2020-07-12 22:38:35 +0300

    #ios-13-app-development-bootcamp # Section 17: SwiftUI and Declarative Programming # 232. H4X0R News Part 4 - Using UIKit Components with SwiftUI

---
+++ Courses/iOS-13-Swift-5-The-Complete-iOS-App-Development-Bootcamp/Development/H4X0R News SUI/H4X0R News SUI/View/DetailView.swift
@@ -0,0 +1,24 @@
+//
+//  DetailView.swift
+//  H4X0R News SUI
+//
+//  Created by Serhii Onyshchenko on 12.07.2020.
+//  Copyright © 2020 Serhii Onyshchenko. All rights reserved.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    
+    let url: String?
+    
+    var body: some View {
+        WebView(urlString: url)
+    }
+}
+
+struct DetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        DetailView(url: "https://www.google.com")
+    }
+}
