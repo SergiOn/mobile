59de98ded2b88c2ddf5b1edd0ebf675793b21233
SergiOn <onishhenko@gmail.com>
2020-07-12 17:06:32 +0300

    #ios-13-app-development-bootcamp # Section 17: SwiftUI and Declarative Programming # 221. How to Build a SwiftUI App from Scratch

---
+++ Courses/iOS-13-Swift-5-The-Complete-iOS-App-Development-Bootcamp/Development/I Am Rich SUI/I Am Rich SUI/ContentView.swift
@@ -0,0 +1,35 @@
+//
+//  ContentView.swift
+//  I Am Rich SUI
+//
+//  Created by Serhii Onyshchenko on 12.07.2020.
+//  Copyright © 2020 Serhii Onyshchenko. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        ZStack {
+            Color(.systemTeal)
+                .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+            VStack {
+                Text("I Am Rich")
+                    .font(.system(size: 40))
+                    .fontWeight(.bold)
+                    .foregroundColor(Color.white)
+                Image("diamond")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 200, height: 200, alignment: .center)
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+//        ContentView().previewDevice(PreviewDevice(rawValue: "iPhone Xs Max"))
+    }
+}
