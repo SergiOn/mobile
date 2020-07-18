4931772a8aa9071d5ec1a11af14142d4493fe63d
SergiOn <onishhenko@gmail.com>
2020-07-18 22:18:07 +0300

    #ios-13-app-development-bootcamp # Section 19: Local Data Persistance - User Defaults, Core Data and Realm # 253. Creating a Textfield Inside UIAlert to Add New Items

--- Courses/iOS-13-Swift-5-The-Complete-iOS-App-Development-Bootcamp/Development/Todoey-iOS13/Todoey/TodoListViewController.swift
+++ Courses/iOS-13-Swift-5-The-Complete-iOS-App-Development-Bootcamp/Development/Todoey-iOS13/Todoey/TodoListViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class TodoListViewController: UITableViewController {
 
-    let itemArray = ["Find Mike", "Buy Eggos", "Destroy Demogorgon"]
+    var itemArray = ["Find Mike", "Buy Eggos", "Destroy Demogorgon"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -37,6 +37,23 @@ class TodoListViewController: UITableViewController {
         print(indexPath.row)
         print(indexPath)
         print(itemArray[indexPath.row])
+    }
+
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        var textField = UITextField()
+        
+        let alert = UIAlertController(title: "Add New Todoey Item", message: "", preferredStyle: .alert)
+        let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
+            self.itemArray.append(textField.text!)
+            self.tableView.reloadData()
+            textField = UITextField()
+        }
+        alert.addTextField { (alertTextField) in
+            alertTextField.placeholder = "Create New Item"
+            textField = alertTextField
+        }
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
     }
 
 }
