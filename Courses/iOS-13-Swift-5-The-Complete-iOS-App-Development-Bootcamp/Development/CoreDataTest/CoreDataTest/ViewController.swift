ebd76e61e3f8bf01c7cdd31df41845de82e35ea8
SergiOn <onishhenko@gmail.com>
2020-07-19 21:13:47 +0300

    #ios-13-app-development-bootcamp # Section 19: Local Data Persistance - User Defaults, Core Data and Realm # 266. How to Set up and Configure Core Data

---
+++ Courses/iOS-13-Swift-5-The-Complete-iOS-App-Development-Bootcamp/Development/CoreDataTest/CoreDataTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoreDataTest
+//
+//  Created by Serhii Onyshchenko on 19.07.2020.
+//  Copyright © 2020 Serhii Onyshchenko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
