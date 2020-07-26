481156857c354872966b82f3a14b6cf074dd5306
SergiOn <onishhenko@gmail.com>
2020-07-26 18:46:38 +0300

    #ios-13-app-development-bootcamp # Section 22: Advanced Swift Classroom - Part 2 # 326. Adding More Functionality to Our Calculator

--- Courses/iOS-13-Swift-5-The-Complete-iOS-App-Development-Bootcamp/Development/Calculator-Advanced-Swift-iOS13/Calculator/ViewController.swift
+++ Courses/iOS-13-Swift-5-The-Complete-iOS-App-Development-Bootcamp/Development/Calculator-Advanced-Swift-iOS13/Calculator/ViewController.swift
@@ -19,7 +19,19 @@ class ViewController: UIViewController {
         //What should happen when a non-number button is pressed
         isFinishedTypingNumber = true
         
-        let number = Double(displayLabel.text!) ?? 0        
+        guard let number = Double(displayLabel.text!) else {
+            fatalError("Cannot convert display label text to a Double.")
+        }
+        
+        if let calcMethod = sender.currentTitle {
+            if calcMethod == "+/-" {
+                displayLabel.text = String(number * -1)
+            } else if calcMethod == "AC" {
+                displayLabel.text = "0"
+            } else if calcMethod == "%" {
+                displayLabel.text = String(number / 100)
+            }
+        }
     }
 
     
