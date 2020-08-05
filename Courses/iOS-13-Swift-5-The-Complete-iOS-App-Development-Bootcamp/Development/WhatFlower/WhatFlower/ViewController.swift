9f1f5455bfaf57f4f68aa85b5a0f533b84f50bd6
SergiOn <onishhenko@gmail.com>
2020-08-05 09:50:40 +0300

    #ios-13-app-development-bootcamp # Section 24: Advanced CoreML - CoreML Tools & Converting Models # 355. [Solution] - Setting up a UIImagePickerController

--- Courses/iOS-13-Swift-5-The-Complete-iOS-App-Development-Bootcamp/Development/WhatFlower/WhatFlower/ViewController.swift
+++ Courses/iOS-13-Swift-5-The-Complete-iOS-App-Development-Bootcamp/Development/WhatFlower/WhatFlower/ViewController.swift
@@ -8,13 +8,33 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    @IBOutlet weak var imageView: UIImageView!
+    
+    let imagePicker = UIImagePickerController()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        imagePicker.delegate = self
+//        imagePicker.sourceType = .camera
+        imagePicker.sourceType = .photoLibrary
+//        imagePicker.allowsEditing = false
+        imagePicker.allowsEditing = true
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+//        let userPickedImage = info[UIImagePickerController.InfoKey.originalImage]
+        let userPickedImage = info[UIImagePickerController.InfoKey.editedImage]
+        imageView.image = userPickedImage as? UIImage
+        
+        imagePicker.dismiss(animated: true, completion: nil)
     }
 
-
+    @IBAction func cameraTapped(_ sender: UIBarButtonItem) {
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
 }
 
