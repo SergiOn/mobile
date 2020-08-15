693d7f74d30cc99fdb2a87b8159940985da78972
SergiOn <onishhenko@gmail.com>
2020-08-15 17:54:05 +0300

    #ios-13-app-development-bootcamp # Section 26: CreateML & Natural Language Processing (NLP) # 381. How to Fetch Live Twitter Data

--- Courses/iOS-13-Swift-5-The-Complete-iOS-App-Development-Bootcamp/Development/Twittermenti-iOS13/Twittermenti/ViewController.swift
+++ Courses/iOS-13-Swift-5-The-Complete-iOS-App-Development-Bootcamp/Development/Twittermenti-iOS13/Twittermenti/ViewController.swift
@@ -20,7 +20,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        swifter.searchTweet(using: "@Apple", success: { (results, metadata) in
+        swifter.searchTweet(using: "@Apple", lang: "en", count: 100, tweetMode: .extended, success: { (results, metadata) in
             print(results)
         }) { (error) in
             print("There was an error with the Twitter API Request, \(error)")
