11c0e1933c1a3e09e5ee188990551837e7dae83d
SergiOn <onishhenko@gmail.com>
2020-08-15 21:20:29 +0300

    #ios-13-app-development-bootcamp # Section 26: CreateML & Natural Language Processing (NLP) # 383. Parsing Tweet Data in JSON

--- Courses/iOS-13-Swift-5-The-Complete-iOS-App-Development-Bootcamp/Development/Twittermenti-iOS13/Twittermenti/ViewController.swift
+++ Courses/iOS-13-Swift-5-The-Complete-iOS-App-Development-Bootcamp/Development/Twittermenti-iOS13/Twittermenti/ViewController.swift
@@ -9,6 +9,7 @@
 import UIKit
 import Swifter
 import CoreML
+import SwiftyJSON
 
 class ViewController: UIViewController {
     
@@ -24,11 +25,25 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         
 //        let prediction = try! sentimentClassifier.prediction(text: "@Apple is a terrible company!")
-        let prediction = try! sentimentClassifier.prediction(text: "@Apple is the best company!")
-        print(prediction.label)
+//        let prediction = try! sentimentClassifier.prediction(text: "@Apple is the best company!")
+//        print(prediction.label)
         
         swifter.searchTweet(using: "@Apple", lang: "en", count: 100, tweetMode: .extended, success: { (results, metadata) in
 //            print(results)
+            
+//            if let tweet = results[0]["full_text"].string {
+//                print(tweet)
+//            }
+            
+//            let tweets = results.array.map { (item) -> String in
+//                return item["full_text"].string
+//            }
+            
+            let tweets = results.array!.map({ (item) -> String in
+                return item["full_text"].string!
+            })
+            print(tweets)
+            
         }) { (error) in
             print("There was an error with the Twitter API Request, \(error)")
         }
