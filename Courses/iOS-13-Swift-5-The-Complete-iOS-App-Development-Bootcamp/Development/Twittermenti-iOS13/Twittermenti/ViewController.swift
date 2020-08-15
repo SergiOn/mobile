e0634e27c2fba8962678fda9a098b150edc8d38c
SergiOn <onishhenko@gmail.com>
2020-08-15 16:12:15 +0300

    #ios-13-app-development-bootcamp # Section 26: CreateML & Natural Language Processing (NLP) # 380. Setting Up the Swifter Framework

--- Courses/iOS-13-Swift-5-The-Complete-iOS-App-Development-Bootcamp/Development/Twittermenti-iOS13/Twittermenti/ViewController.swift
+++ Courses/iOS-13-Swift-5-The-Complete-iOS-App-Development-Bootcamp/Development/Twittermenti-iOS13/Twittermenti/ViewController.swift
@@ -7,16 +7,24 @@
 //
 
 import UIKit
+import Swifter
 
 class ViewController: UIViewController {
     
     @IBOutlet weak var backgroundView: UIView!
     @IBOutlet weak var textField: UITextField!
     @IBOutlet weak var sentimentLabel: UILabel!
-
+    
+    let swifter = Swifter(consumerKey: "rsalS0xap2oUiRlQHweL2cZv8", consumerSecret: "FbdEbYKXVoxtuQhGDWo2kV7l2XBbLxFuFcXhZFfv4vvBSpGnKk")
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        swifter.searchTweet(using: "@Apple", success: { (results, metadata) in
+            print(results)
+        }) { (error) in
+            print("There was an error with the Twitter API Request, \(error)")
+        }
     }
 
     @IBAction func predictPressed(_ sender: Any) {
