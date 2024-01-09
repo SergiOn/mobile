16ab45c27cf02a2227ad503b02e8bab898ac58ac
SergiOn <onishhenko@gmail.com>
2024-01-09 20:59:03 +0200

    #letsbuildthatapp-swiftui-mastery-travel-discovery # created project

---
+++ letsbuildthatapp/TravelDiscovery/TravelDiscovery/TravelDiscoveryApp.swift
@@ -0,0 +1,17 @@
+//
+//  TravelDiscoveryApp.swift
+//  TravelDiscovery
+//
+//  Created by Serhii Onyshchenko on 09.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct TravelDiscoveryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
