7755080f433d088846739170f1b296b0139bdf67
SergiOn <onishhenko@gmail.com>
2024-01-10 20:37:51 +0200

    #letsbuildthatapp-swiftui-mastery-travel-discovery # Home Screen Layout # 1. NavigationView ScrollViews and Stacking

--- letsbuildthatapp/TravelDiscovery/TravelDiscovery/ContentView.swift
+++ letsbuildthatapp/TravelDiscovery/TravelDiscovery/ContentView.swift
@@ -5,20 +5,44 @@
 //  Created by Serhii Onyshchenko on 09.01.2024.
 //
 
+// NavigationView is deprecated
+// https://developer.apple.com/documentation/swiftui/navigationview
+
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationView {
+            ScrollView {
+                DiscoverCategoriesView()
+            }
+            .navigationTitle("Discover")
         }
-        .padding()
     }
 }
 
 #Preview {
     ContentView()
+}
+
+struct DiscoverCategoriesView: View {
+    var body: some View {
+        ScrollView(.horizontal, showsIndicators: false) {
+            HStack(spacing: 8) {
+                ForEach(0..<5, id: \.self) { num in
+                    VStack(spacing: 4) {
+                        Spacer()
+                            .frame(width: 50, height: 50)
+                            .background(Color.gray)
+                            .cornerRadius(.infinity)
+                            .shadow(color: .gray, radius: 4, x: 0, y: 2)
+                        Text("Art")
+                            .font(.system(size: 12, weight: .semibold))
+                    }
+                }
+            }
+            .padding(.horizontal)
+        }
+        // .background(Color.red)
+    }
 }
