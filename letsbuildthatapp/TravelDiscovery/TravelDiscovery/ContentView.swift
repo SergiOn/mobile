fbdc06285d5114dfafc23abc29e18c0a517ecd18
SergiOn <onishhenko@gmail.com>
2024-01-12 22:26:29 +0200

    #letsbuildthatapp-swiftui-mastery-travel-discovery # Home Screen Layout # 3. Destinations Row Images and Sizing

--- letsbuildthatapp/TravelDiscovery/TravelDiscovery/ContentView.swift
+++ letsbuildthatapp/TravelDiscovery/TravelDiscovery/ContentView.swift
@@ -67,7 +67,20 @@ struct DiscoverCategoriesView: View {
     }
 }
 
+struct Destination: Hashable {
+    let name: String
+    let country: String
+    let imageName: String
+}
+
 struct PopularDestinationsView: View {
+    
+    let destinations: [Destination] = [
+        .init(name: "Paris", country: "France", imageName: "eiffel_tower"),
+        .init(name: "Tokyo", country: "Japan", imageName: "japan"),
+        .init(name: "New York", country: "US", imageName: "new_york")
+    ]
+    
     var body: some View {
         VStack {
             HStack {
@@ -82,13 +95,33 @@ struct PopularDestinationsView: View {
 
             ScrollView(.horizontal) {
                 HStack(spacing: 8) {
-                    ForEach(0..<5, id: \.self) { num in
-                        Spacer()
-                            .frame(width: 125, height: 150)
-                            .background(Color.gray)
-                            .cornerRadius(5)
-                            .shadow(color: .gray, radius: 4, x: 0, y: 2)
-                            .padding(.bottom)
+                    ForEach(destinations, id: \.self) { destination in
+//                        Spacer()
+                        VStack(alignment: .leading, spacing: 0) {
+                            Image(destination.imageName)
+                                .resizable()
+                                .scaledToFill()
+                                .frame(width: 125, height: 125)
+//                                .clipped()
+                                .cornerRadius(4)
+                                .padding(.horizontal, 6)
+                                .padding(.vertical, 6)
+
+                            Text(destination.name)
+                                .font(.system(size: 12, weight: .semibold))
+                                .padding(.horizontal, 12)
+
+                            Text(destination.country)
+                                .font(.system(size: 12, weight: .semibold))
+                                .padding(.horizontal, 12)
+                                .padding(.bottom, 8)
+                                .foregroundColor(.gray)
+                        }
+//                        .frame(width: 125)
+                        .background(Color(.init(white: 0.9, alpha: 1)))
+                        .cornerRadius(5)
+                        .shadow(color: .gray, radius: 4, x: 0, y: 2)
+                        .padding(.bottom)
                     }
                 }
                 .padding(.horizontal)
